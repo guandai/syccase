71780177e25fc21e28a37147342327d4eb2e9d1c
twindai <guandai@hotmail.com>
2025-04-26 00:48:26 +0200

    add basic iles

--- syccase_addon.php
+++ syccase_addon.php
@@ -9,9 +9,11 @@ Author URI: https://twindai.com
 License: GPL2
 */
 
-
 // Enqueue a custom script
 function my_custom_plugin_scripts() {
     wp_enqueue_script('my-custom-script', plugins_url('js/custom-script.js', __FILE__), array('jquery'), '1.0', true);
+
+    wp_enqueue_style('my-custom-style', plugins_url('css/custom-style.css', __FILE__), array(), '1.0');
+    
 }
 add_action('wp_enqueue_scripts', 'my_custom_plugin_scripts');
